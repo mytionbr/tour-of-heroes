2f4715bcc1ff53d6c52c9ebb0566e2db9e0d16d9
matheus jesus <matheuss.jesus2017@gmail.com>
2022-02-17 10:31:40 -0300

    test

--- src/database.ts
+++ src/database.ts
@@ -4,6 +4,7 @@ import config from "./config";
 
 export const connect = async (): Promise<void> => {
     const { ormconfig } = config
+    console.log(ormconfig)
     await createConnection(ormconfig);
     console.log('Connected database');  
 }
