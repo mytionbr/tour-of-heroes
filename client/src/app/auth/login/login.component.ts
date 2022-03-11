412bf530c00e210ee8f8adfb835465ee3a92d91e
matheus jesus <matheuss.jesus2017@gmail.com>
2022-03-11 11:08:36 -0300

    add validation in login form

--- client/src/app/auth/login/login.component.ts
+++ client/src/app/auth/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../auth.service';
 
 @Component({
@@ -9,16 +9,41 @@ import { AuthService } from '../auth.service';
 })
 export class LoginComponent implements OnInit {
 
-  form: FormGroup;
+  loginForm: FormGroup;
+  submitted: boolean = false;
 
-  constructor(private authService: AuthService, private formBuilder: FormBuilder) { 
-    this.form = this.formBuilder.group({
-      email: [null],
-      password: [null]
+  constructor(private authService: AuthService, private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.minLength(6),Validators.required]]
     })
   }
 
   ngOnInit(): void {
+  }
+
+  get loginFormControl() {
+    return this.loginForm.controls;
+  }
+
+  verifyValidRequired(property: string) {
+    return (this.loginFormControl[property].touched || this.submitted) && this.loginFormControl[property].errors?.['required']
+  }
+
+  verifyValidEmail() {
+    return this.loginFormControl['email'].touched && this.loginFormControl['email'].dirty && this.loginFormControl['email'].errors?.['email']
+  }
+
+  verifyValidPassword() {
+    console.log(this.loginFormControl['password'].errors?.['minlength'])
+    return this.loginFormControl['password'].touched && this.loginFormControl['password'].dirty && this.loginFormControl['password'].errors?.['minlength']
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.loginForm.valid) {
+      alert('Login efetuado com sucesso!')
+    }
   }
 
 }
